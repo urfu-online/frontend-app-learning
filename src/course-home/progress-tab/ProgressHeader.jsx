90189b845686580f3f3fd64269f99e4ac6143da3
MasterGowen <mastergowen@gmail.com>
2022-09-21 18:26:33 +0500

    ru translations, progress header

--- src/course-home/progress-tab/ProgressHeader.jsx
+++ src/course-home/progress-tab/ProgressHeader.jsx
@@ -28,7 +28,7 @@ function ProgressHeader({ intl }) {
   return (
     <>
       <div className="row w-100 m-0 mt-3 mb-4 justify-content-between">
-        <h1>{pageTitle}</h1>
+        <h3>{pageTitle}</h3>
         {administrator && studioUrl && (
           <Button variant="outline-primary" size="sm" className="align-self-center" href={studioUrl}>
             {intl.formatMessage(messages.studioLink)}
